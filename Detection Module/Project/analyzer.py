4b60ef3c335b21d9c9646bd6db97229b2cc31114
rasp IC <brenno.tondato@gmail.com>
2016-08-04 22:17:57 -0300

    some errors tha i dont know fixed

--- Detection Module/Project/analyzer.py
+++ Detection Module/Project/analyzer.py
@@ -7,10 +7,7 @@
 import numpy as np
 import cv2
 import math
-from matplotlib import pyplot as plt
 
-
-# funcao para subitrair o fundo
 def backgroundSubstraction(back, frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     diff_frame = cv2.absdiff(back, frame)
@@ -28,8 +25,7 @@ def histogram(diff_frame):
     print hist
 
 
-"""
-def shannonEntropy(Img_histogram):
+"""def shannonEntropy(Img_histogram):
     SE = 0
 
     for i in Img_histogram:
