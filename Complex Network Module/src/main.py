ccb94ea7188dedc52d92ea19d5cb9c9b8116a70f
Brenno Tondato de Faria <brenno.tondato@gmail.com>
2017-05-28 00:23:28 -0300

    Building an crawler on main.py

--- Complex Network Module/src/main.py
+++ Complex Network Module/src/main.py
@@ -1,1 +1,21 @@
-#!
+#!/usr/bin/python
+
+
+import logging
+from grab.spider import Spider, Task
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    class ExampleSpider(Spider):
+        def task_generator(self):
+            for lang in 'python', 'ruby', 'perl':
+                url = 'https://www.google.com/search?q=%s' % lang
+                yield Task('search', url=url, lang=lang)
+
+        def task_search(self, grab, task):
+             print('%s: %s' % (task.lang, grab.doc('//div[@class="s"]//cite').text()))
+
+bot = ExampleSpider(thread_number=2)
+bot.run()
